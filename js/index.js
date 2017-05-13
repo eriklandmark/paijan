11e022a67e6f6c06e2d38df7f916cac9b6577ac5
itggot-erik-landmark <erik.landmark@itggot.se>
2017-05-13 19:33:29 +0200

    Fixade animationen för laddning av sidor

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
+var buffer = "";
+var load_content = false;
+var name_of_page = "home";
+var is_loading = false;
+
 window.addEventListener("load", function () {
+    getPageContent("home");
+    var loading_screen = document.getElementById("loading_div");
     setTimeout(function () {
-        loadPage("home", document.getElementById("nav_title_home"));
+        if (buffer !== null || buffer !== "") {
+            document.getElementById("loaded_page").innerHTML = buffer;
+            loading_screen.className = "fade_out_animation";
+            loading_screen.style.opacity = "0";
+            load_content = false;
+        } else {
+            load_content = true;
+        }
     }, 500);
 });
 
+document.getElementById("loading_div").addEventListener("animationend", function () {
+    if(is_loading) {
+        if (buffer !== null || buffer !== "") {
+            document.getElementById("loaded_page").innerHTML = buffer;
+            var loading_screen = document.getElementById("loading_div");
+            loading_screen.className = "fade_out_animation";
+            loading_screen.style.opacity = "0";
+            load_content = false;
+        } else {
+            load_content = true;
+        }
+        is_loading = false;
+    }
+});
+
 function loadPage(page_name, element) {
+    getPageContent(page_name);
+    name_of_page = page_name;
+    var loading_screen = document.getElementById("loading_div");
+    loading_screen.style.animationPlayState = "initial";
+    loading_screen.className = "fade_in_animation";
+    loading_screen.style.opacity = "1";
+    is_loading = true;
+
     var titles = document.getElementsByClassName("nav_title");
     for(var i = 0; i < titles.length; i++) {
         if (titles[i] !== element) {
@@ -14,30 +51,32 @@ function loadPage(page_name, element) {
     if (element !== null) {
         element.style.fontWeight = "bold";
     }
-    var request = new XMLHttpRequest();
-    document.getElementById("loaded_page").innerHTML = "";
-    document.getElementById("loading_gif").style.display = "flex";
-    document.getElementsByTagName("footer")[0].style.display = "none";
     document.title = "Paijan - " + page_name;
-    setTimeout(function () {
-        request.onreadystatechange = function() {
-            if (this.readyState === 4) {
-                if ( this.status === 200) {
-                    document.getElementById("loading_gif").style.display = "none";
-                    document.getElementById("loaded_page").innerHTML = this.responseText;
-                    if(Math.max( document.body.scrollHeight, document.body.offsetHeight) > window.innerHeight) {
-                        document.getElementsByTagName("footer")[0].style.position = "relative";
+}
+
+function getPageContent(page_name) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            if ( this.status === 200) {
+                if(name_of_page === page_name) {
+                    if (load_content === true) {
+                        document.getElementById("loaded_div").innerHTML = this.responseText;
+                        var loading_screen = document.getElementById("loading_div");
+                        loading_screen.className = "fade_out_animation";
+                        loading_screen.style.opacity = "0";
+                        load_content = false;
                     } else {
-                        document.getElementsByTagName("footer")[0].style.position = "absolute";
+                        buffer = this.responseText;
                     }
-                    document.getElementsByTagName("footer")[0].style.display = "flex";
-                    console.log("Loading page: '" + page_name + "'");
                 } else {
-                    console.log("Error loading page: '" + page_name + "'");
+                    getPageContent(name_of_page);
                 }
+            } else {
+                console.log("Error loading page: '" + page_name + "'");
             }
-        };
-        request.open("GET", "sites/" + page_name + ".html", true);
-        request.send();
-    }, Math.floor((Math.random()/2.0 * 1000) + 200));
+        }
+    };
+    request.open("GET", "sites/" + page_name + ".html", true);
+    request.send();
 }
