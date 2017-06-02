0da740966cd5920f37d282636fed1d0c24c9cc4a
Erik Landmark <erik.landmark@itggot.se>
2017-06-03 00:05:09 +0200

    Klar med koden! Lade till en min för javascripten.

--- js/lib.js
+++ js/lib.js
@@ -5,3 +5,15 @@ String.prototype.replaceAll = function (search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
+
+function msg(msg) {
+    var msg_div = document.getElementById("info_msg");
+    var msg_element = document.getElementById("info_msg_text");
+    msg_div.style.display = "flex";
+    msg_element.innerHTML = msg;
+}
+
+function close_msg_box() {
+    var msg_div = document.getElementById("info_msg");
+    msg_div.style.display = "none";
+}
