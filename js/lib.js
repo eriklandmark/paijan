dadee50bb69b1a489437ee184726f4181b731a9e
Erik Landmark <erik.landmark@itggot.se>
2017-06-03 01:44:15 +0200

    Uppdaterade de sista filerna.

--- js/lib.js
+++ js/lib.js
@@ -7,13 +7,13 @@ String.prototype.replaceAll = function (search, replacement) {
 };
 
 function msg(msg) {
-    var msg_div = document.getElementById("info_msg");
-    var msg_element = document.getElementById("info_msg_text");
+    var msg_div = document.getElementById("modal_box");
+    var msg_element = document.getElementById("modal_box_text");
     msg_div.style.display = "flex";
     msg_element.innerHTML = msg;
 }
 
 function close_msg_box() {
-    var msg_div = document.getElementById("info_msg");
+    var msg_div = document.getElementById("modal_box");
     msg_div.style.display = "none";
 }
